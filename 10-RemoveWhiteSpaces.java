33fcd4eb705d0e59b4d4da9886349675083ceaa4
Sharad Singh Kushwaha <153629616+absolutely-sharad@users.noreply.github.com>
2024-06-17 23:52:31 +0530

    Create 10-RemoveWhiteSpaces.java

---
+++ 10-RemoveWhiteSpaces.java
@@ -0,0 +1,11 @@
+public class RemoveWhitespace {
+    public static void main(String[] args) {
+        String input = "This is a   string with    multiple spaces and \t tabs.";
+        
+        // Using replaceAll to remove all whitespace characters
+        String output = input.replaceAll("\\s+", "");
+        
+        System.out.println("Original string: " + input);
+        System.out.println("String without whitespaces: " + output);
+    }
+}
